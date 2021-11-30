1107c5023c4a949842d715e820867053b7ed3000
Tuan phan <you@example.com>
2021-11-30 21:32:28 +0700

    TuanPhan2000
    update Driver

--- BackEnd/src/main/java/com/team12/btl/controller/DriverController.java
+++ BackEnd/src/main/java/com/team12/btl/controller/DriverController.java
@@ -1,9 +1,14 @@
 package com.team12.btl.controller;
 
+import com.team12.btl.entity.Driver;
+import com.team12.btl.entity.Route;
 import com.team12.btl.service.service_impl.DriverService_Impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/driver")
@@ -15,6 +20,30 @@ public class DriverController {
     @GetMapping("/findAll")
     public ResponseEntity<?> findAll() throws Exception {
         return ResponseEntity.ok(driverService.findAll());
+    }
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<?> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok(driverService.findById(id));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createDriver(@Valid @RequestBody Driver driver) {
+        return ResponseEntity.ok(driverService.create(driver));
+    }
+
+    @DeleteMapping("/deleteById/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Integer id) {
+        return ResponseEntity.ok(driverService.delete(id));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> update(@Valid @RequestBody Driver driver) {
+        return ResponseEntity.ok(driverService.update(driver));
+    }
+
+    @GetMapping("/searchDriver")
+    public ResponseEntity<?> searchDriver(@RequestParam Map<String, String> param) {
+        return ResponseEntity.ok(driverService.searchDriver(param));
     }
 
 }
--- BackEnd/src/main/java/com/team12/btl/repository/DriverRepository.java
+++ BackEnd/src/main/java/com/team12/btl/repository/DriverRepository.java
@@ -1,9 +1,33 @@
 package com.team12.btl.repository;
 
 import com.team12.btl.entity.Driver;
+import com.team12.btl.entity.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DriverRepository extends JpaRepository<Driver, Integer>, JpaSpecificationExecutor<Driver> {
+    List<Driver> findByActiveIsTrue();
 
+    Driver findByIdAndActiveTrue(Integer id);
+
+    List<Driver> findByActiveIsTrueOrderById();
+
+    @Modifying
+    @Query(value = "UPDATE driver SET is_active = 0 WHERE Id = ?1", nativeQuery = true)
+    int deleteDriver(Integer Id);
+
+    @Query(value = "SELECT d.* FROM driver d where d.is_active = true AND (CONCAT(d.id, '') LIKE %?1%) AND UPPER(d.name) like CONCAT('%',UPPER(?2),'%') " +
+            "AND UPPER(d.id_card) LIKE CONCAT('%',UPPER(?3),'%') " +
+            "AND (CONCAT(d.driving_license_code, '') LIKE %?4%)" +
+            "AND (CONCAT(d.type_of_license, '') LIKE %?5%)" +
+            "AND (CONCAT(d.address, '') LIKE %?6%)" +
+            "AND (CONCAT(d.birthday, '') LIKE %?7%)" +
+            "AND (CONCAT(d.experience, '') LIKE %?8%)" +
+            "AND (CONCAT(d.fixed_salary_id, '') LIKE %?9%)"
+            , nativeQuery = true)
+    List<Driver> searchDriver(String id, String name, String idCard, String drivingLicenseCode, String typeOfLicense, String address, String birthday, String experience, String fixed_salary_id);
 }
