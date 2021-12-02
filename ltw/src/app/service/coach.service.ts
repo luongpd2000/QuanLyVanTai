b74c7833a8921469b917c019bcd7bb678e78906a
khuatduykhanh179 <80501915+khuatduykhanh179@users.noreply.github.com>
2021-12-02 15:50:14 +0700

    xong 2 file ts

--- ltw/src/app/service/coach.service.ts
+++ ltw/src/app/service/coach.service.ts
@@ -1,9 +1,53 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { environment } from 'src/environments/environment';
+import { RequestParam } from '../data/request-param';
+import { Coach } from '../data/coach';
+import { ApiUrlUtil } from '../util/api-url.util';
+import { ParamUtil } from '../util/param.util';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoachService {
+  private baseUrl = `${environment.baseUrl}/coach`;
+  private httpOptions = {
+    headers: new HttpHeaders(
+      { 'Content-Type': 'application/json' ,
+        'Authorization': this._cookieService.get('Authorization')
+      })
+  };
+  constructor(private httpClient: HttpClient,
+              private _cookieService: CookieService) { }
 
-  constructor() { }
+  getAll(){
+    const coachUrl = `${this.baseUrl}/findAll`;
+    return this.httpClient.get<Coach[]>(coachUrl,this.httpOptions);
+  }
+  getCoachById(id: number){
+    const coachUrl = `${this.baseUrl}/findById/${id}`;
+    return this.httpClient.get<Coach>(coachUrl,this.httpOptions);
+  }
+  createCoach(coach: Coach){
+    const coachUrl = `${this.baseUrl}/create`;
+    return this.httpClient.post<Coach>(coachUrl,coach,this.httpOptions);
+  }
+  searchCoach(param: any){
+    const params: RequestParam[] = ParamUtil.toRequestParams(param);
+    const coachUrl = ApiUrlUtil.buildQueryString(this.baseUrl+ '/searchCoach', params);
+    console.log(coachUrl);
+
+    return this.httpClient.get<Coach[]>(coachUrl,this.httpOptions);
+  }
+  deleteCoach(id: number){
+    const coachUrl = `${this.baseUrl}/deleteById/${id}`;
+    return this.httpClient.delete<any>(coachUrl,this.httpOptions);
+  }
+
+  update(coach: Coach){
+    const coachUrl = `${this.baseUrl}/update`;
+    return this.httpClient.put<any>(coachUrl,coach,this.httpOptions);
+  }
 }
