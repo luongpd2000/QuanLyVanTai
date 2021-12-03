e8cc921fc4f39fa3b1998bb8e36658576891a106
Tuan phan <you@example.com>
2021-12-03 10:31:52 +0700

    API finAll() Bac Luong

--- BackEnd/src/main/java/com/team12/btl/repository/FixedSalaryRepository.java
+++ BackEnd/src/main/java/com/team12/btl/repository/FixedSalaryRepository.java
@@ -1,9 +1,12 @@
 package com.team12.btl.repository;
 
 import com.team12.btl.entity.FixedSalary;
+import com.team12.btl.entity.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface FixedSalaryRepository extends JpaRepository<FixedSalary, Integer>, JpaSpecificationExecutor<FixedSalary> {
+import java.util.List;
 
+public interface FixedSalaryRepository extends JpaRepository<FixedSalary, Integer>, JpaSpecificationExecutor<FixedSalary> {
+    List<FixedSalary> findAll();
 }
