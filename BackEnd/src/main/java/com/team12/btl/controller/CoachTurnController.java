cc1e2ba4ec3e5fd558da6ec47ff714b1e851b278
duynn9433 <92941533+duynn9433@users.noreply.github.com>
2021-12-05 20:53:50 +0700

    coach turn v1.0

--- BackEnd/src/main/java/com/team12/btl/controller/CoachTurnController.java
+++ BackEnd/src/main/java/com/team12/btl/controller/CoachTurnController.java
@@ -5,6 +5,7 @@ import com.team12.btl.entity.CoachTurn;
 import com.team12.btl.entity.Route;
 import com.team12.btl.service.service_impl.CoachTurnService_Impl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
@@ -33,7 +34,13 @@ public class CoachTurnController {
 
     @PutMapping("/update")
     public ResponseEntity<?> update(@Valid @RequestBody CoachTurn coachTurn){
-        return ResponseEntity.ok(coachTurnService.update(coachTurn));
+        CoachTurn res = coachTurnService.update(coachTurn);
+        if(res != null) {
+            return ResponseEntity.ok(coachTurnService.update(coachTurn));
+        } else{
+            System.out.println("\u001B[31m" + "Driver and Assistant must different && startDate < endDate" +"\u001B[0m");
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @DeleteMapping("/deleteById/{Id}")
