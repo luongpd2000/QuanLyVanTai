d49fae26b8316fcbb1cfff94531b39b7ea075655
luongpd2000 <52768136+luongpd2000@users.noreply.github.com>
2021-12-11 21:11:46 +0700

    Update CoachRepository.java

--- BackEnd/src/main/java/com/team12/btl/repository/CoachRepository.java
+++ BackEnd/src/main/java/com/team12/btl/repository/CoachRepository.java
@@ -25,8 +25,8 @@ public interface CoachRepository extends JpaRepository<Coach, Integer>, JpaSpeci
     @Query(value="SELECT c.* FROM coach c where c.is_active = true AND (CONCAT(c.id, '') LIKE %?1%) AND UPPER(c.plate) like CONCAT('%',UPPER(?2),'%') " +
             "AND UPPER(c.model) LIKE CONCAT('%',UPPER(?3),'%') " +
             "AND UPPER(c.manufacturer) LIKE CONCAT('%',UPPER(?4),'%') " +
-            "AND (c.capacity >= ?5)" +
-            "AND (c.years_of_use <= ?6)"+
+            "AND (c.capacity >= ?5) " +
+            "AND (c.years_of_use <= ?6 OR ?6 = '') "+
             "AND (CONCAT(c.last_maintenance_day, '') LIKE %?7%)" , nativeQuery = true)
     List<Coach> searchCoach(String id,String plate,String model, String manufacturer, String capacity, String yearsOfUse,String lastMaintenanceDay);
 }
